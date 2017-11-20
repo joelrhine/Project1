e9a2c087f60a4f9221e0457df3bb9d32ea72cabf
Joel Rhine <joelrhine@gwu.edu>
2017-11-20 00:39:16 -0500

    Made changes to tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -37,6 +37,7 @@ func1(10,20)
 print (func1(5,6))
 print (func1)
 
+
 def func2(x):
     cube = x*x*x
     return cube
@@ -59,3 +60,45 @@ def multi_list(*args):
     return result
 
 print (multi_list(4,5,6,7))
+
+# Classes
+
+class eighties():
+    def jazz(self):
+        print ("This is jazz music")
+
+    def rock_n_roll(self):
+        print ("rock_n_roll")
+
+class nineties(eighties):
+    def pop(self):
+        eighties.rock_n_roll(self)
+
+    def pop_rock(self):
+        print ("pop_rock")
+
+
+def main():
+    m = eighties()
+    m.jazz()
+    m.rock_n_roll()
+    m1 = nineties()
+    m1.pop()
+    m1.pop_rock()
+
+if __name__ == '__main__':
+    main()
+
+# Date time and datetime Classes
+
+from datetime import date
+from datetime import time
+from datetime import datetime
+
+print ("today's date is ", date.today())
+
+print ("Date componantes are", today.day, today.month, today.year)
+
+now = datetime.now()
+
+print("the time right now is ", now)
